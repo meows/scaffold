8952aa28b417672abdded248581751d06f9c7215
meows <meows@users.noreply.github.com>
2024-06-26 08:46:56 -0700

    next config (#3)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // https://nextjs.org/docs/app/api-reference/next-config-js/eslint
+  eslint: { ignoreDuringBuilds: true },
+  // https://nextjs.org/docs/app/api-reference/next-config-js/webVitalsAttribution
+  // https://web.dev/articles/vitals
+  webVitalsAttribution: ["CLS", "LCP", "INP"],
+}
 
 export default nextConfig
