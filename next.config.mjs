301da86b097d61cb4cfac745f0bcfca1c23b3aed
meow <meow@>
2024-06-26 08:40:44 -0700

    adding experimental next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // https://nextjs.org/docs/app/api-reference/next-config-js/eslint
+  eslint: { ignoreDuringBuilds: true },
+  // https://nextjs.org/docs/app/api-reference/next-config-js/webVitalsAttribution
+  webVitalsAttribution: ["CLS", "LCP"],
+}
 
 export default nextConfig
