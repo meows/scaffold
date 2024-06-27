89df4b8794035be043a3e2f5159fe87d2695cb18
meow <meow@>
2024-06-27 11:23:18 -0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
+/** 
+ * @type {import('next').NextConfig} 
+ * @date 27.06.2024
+ * @note Next appears to be deprecating `experimental.serverComponentsExternalPackages`.
+ * @see https://github.com/vercel/next.js/blob/canary/packages/next/src/lib/server-external-packages.json 
+ */
 const nextConfig = {
   // https://nextjs.org/docs/app/api-reference/next-config-js/eslint
   eslint: { ignoreDuringBuilds: true },
@@ -6,7 +11,7 @@ const nextConfig = {
     // https://nextjs.org/docs/app/api-reference/next-config-js/webVitalsAttribution
     // https://web.dev/articles/vitals
     webVitalsAttribution: ["CLS", "LCP", "INP"],
-  }
+  },
 }
 
 export default nextConfig
