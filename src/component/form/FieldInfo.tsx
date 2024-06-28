4ebf97a3d7ff166311f67fa605cc716350914edd
meow <meow@>
2024-06-27 18:11:43 -0700

    update

--- src/component/form/FieldInfo.tsx
+++ src/component/form/FieldInfo.tsx
@@ -1,5 +1,9 @@
 import type { FieldApi } from "@tanstack/react-form"
 
+/**
+ * Utility component to display TanStack Form field meta information, like error 
+ * and pending.
+ */
 export default function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
   const { touchedErrors, isValidating } = field.state.meta
   return (
