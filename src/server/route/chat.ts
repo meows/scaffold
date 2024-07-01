43482c5beb425fb320ff18caa6bdda6915fd4932
meow <meow@>
2024-06-30 19:01:32 -0700

    update

--- src/server/route/chat.ts
+++ src/server/route/chat.ts
@@ -1,10 +1,20 @@
-import Elysia from "elysia"
+import { eq } from "drizzle-orm"
+import Elysia, { t } from "elysia"
 import { db } from "~/db/client"
 import { Chat } from "~/db/schema"
+import { throwError } from "~/lib/lambda"
 
 const chat = new Elysia({ prefix: "/chat" })
-  .get("/:room", req => {
-    db.select().from(Chat).where({ room: req.params.room })
+  .get("/:room", async (req) => {
+    const chats = await db
+      .select()
+      .from(Chat)
+      .where(eq(Chat.room, Number(req.params.room)))
+      .orderBy(Chat.posted)
+      .run()
+      .catch(throwError)
+
+    return chats
   })
 
 export default chat
