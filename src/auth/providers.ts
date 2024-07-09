c26ad48b5288a83167f1a5927a4f63468b044c59
meow <meow@taiwan>
2024-07-09 11:47:04 -0700

    update

--- src/auth/providers.ts
+++ src/auth/providers.ts
@@ -1,3 +1,4 @@
 import { GitHub, Discord } from "arctic"
 
-export const github = new GitHub(process.env.GITHUB_CLIENT_ID!, process.env.GITHUB_CLIENT_SECRET!)
+export const github = new GitHub(process.env.GITHUB_CLIENT_ID!, process.env.GITHUB_CLIENT_SECRET!, "")
+export const discord = new Discord(process.env.DISCORD_CLIENT_ID!, process.env.DISCORD_CLIENT_SECRET!, "")
