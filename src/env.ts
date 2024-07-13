b9e472f27fa854120bbda938b1911500759296e0
meow <meow@>
2024-07-12 17:37:44 -0700

    update

--- src/env.ts
+++ src/env.ts
@@ -8,6 +8,6 @@ const is_url = pipe(string("Oops, not a string."), url("Oops, not a URL."))
 // —————————————————————————————————————————————————————————————————————————————
 // Environment Variables
 
-export const DATABASE_URL = parse(is_url, process.env.DATABASE_URL)
+export const DATABASE_URL = parse(string(), process.env.DATABASE_URL)
 export const STREAM_URL   = parse(is_url, process.env.STREAM_URL)
 export const SERVER_URL   = parse(is_url, process.env.SERVER_URL)
