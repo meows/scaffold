6b9f9a0880cc6f3a85e45bd08514fbb2eec45c27
meow <meow@>
2024-07-12 17:34:24 -0700

    update

--- src/env.ts
+++ src/env.ts
@@ -6,3 +6,5 @@ import { pipe, parse, string, url } from "valibot"
 const is_url = pipe(string("Oops, not a string."), url("Oops, not a URL."))
 
 export const DATABASE_URL = parse(is_url, process.env.DATABASE_URL)
+export const STREAM_URL   = parse(is_url, process.env.STREAM_URL)
+export const SERVER_URL   = parse(is_url, process.env.SERVER_URL)
