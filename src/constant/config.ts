42ae605cc71ea996e730b4dd8daaf9f6379c20c7
meow <meow@>
2024-08-06 18:55:54 -0700

    caddy

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,5 +1,5 @@
 /** Endpoint for the frontend API client. */
-export const API_PORT = 3000
+export const API_PORT = 3333
 export const API_HOST = "localhost"
-export const API_URL = `http://${API_HOST}:${API_PORT}/api`
-export const API_WEBSOCKET = `ws://${API_HOST}:${API_PORT}/api/ws`
+export const API_URL = `http://${API_HOST}:${API_PORT}/eden`
+export const API_WEBSOCKET = `ws://${API_HOST}:${API_PORT}/eden/ws`
